929ea680fce803ad78748f2eefd125345795b99e
adeogun <adeogunsanya@googlemail.com>
2015-05-20 09:50:39 +0900

    structure change and error handling

--- logintemplate.js
+++ logintemplate.js
@@ -1,10 +1,72 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+	//Turn logging on
+	//verbose: true,
+    //logLevel: "debug"
+});
+
+
 var config = require("config.json").configName;
 var loading = false;
 
-casper.start();
 //Set useragent
 casper.userAgent(config.userAgent);
+
+//Open start page
+casper.start(config.loginPage);
+console.log("Opening login page");
+
+PageReady();
+
+//Enter login info
+casper.then(function step1() {    
+	console.log("Entering Credentials");
+    casper.evaluate(function(username, password) {			
+		var arr = document.getElementsByClassName("form");		
+		arr[0].elements["username"].value = username;
+		arr[0].elements["password"].value = password;
+		
+    },config.username, config.password);
+});
+ 
+//Click login button
+casper.then(function () {  
+    console.log("Clicking login button");
+    casper.evaluate(function () {
+        var arr = document.getElementsByClassName("formname");
+        arr[0].elements["submitbuttonname"].click();
+      });
+});
+
+PageReady();
+
+casper.then(function () {
+    //Download from a form that has no link by parsing form parameters
+    console.log("Downloading CSV");
+    var response = casper.evaluate(function () {
+        var res = {};
+        f = document.forms["form"];
+          //Click submit, get params then cancel submit action
+        f.onsubmit = function () {
+            var post = {};
+            console.log("element size " + f.elements.length);
+            for (i = 0; i < f.elements.length; i++) {
+                post[f.elements[i].name] = f.elements[i].value;
+            }
+            res.action = f.action;
+            res.post = post;
+			return false;
+        }
+
+        var button = document.getElementById("download_csv");
+        button.click();
+        return res;
+    });
+
+      console.log(JSON.stringify(response.post));
+	   var filename = "filename";	
+      //Use parameters to make post
+      casper.download(response.action, filename, "POST", response.post);
+});
 
 //Download resource if you know the URL
 casper.on('resource.received', function (resource) {
@@ -28,75 +90,24 @@ casper.on('load.finished', function () {
     //console.log("load started");
 });
 
-//List of actions to be performed
-var actions = [
-  function () {
-      console.log("Opening login page");
-      casper.open(config.loginPage);
-  },
-  //Enter login info
-  function () {
-      console.log("Entering Credentials");
-       casper.evaluate(function(username, password) {	
-			
-         var arr = document.getElementsByClassName("form");
-		
-		  arr[0].elements["username"].value = username;
-          arr[0].elements["password"].value = password;			
+//Handle error and specify status code
+casper.on('error', function(msg, trace) {
+	console.log("error");
+	console.log(msg);
+	console.log(JSON.stringify(trace));
+	casper.exit(1); 
+});
 
-        },config.username, config.password);
-  },
-  //Click login button
-  function () {
-      //Login
-      console.log("Clicking login button");
-      casper.evaluate(function () {
-          var arr = document.getElementsByClassName("formname");
-          arr[0].elements["submitbuttonname"].click();
-      });
-  },
-  function () {
-      //Download from a form that has no link by parsing form parameters
-      console.log("Downloading CSV");
-      var response = casper.evaluate(function () {
-          var res = {};
-          f = document.forms["form"];
-          //Click submit, get params then cancel submit action
-          f.onsubmit = function () {
-              var post = {};
-              console.log("element size " + f.elements.length);
-              for (i = 0; i < f.elements.length; i++) {
-                  post[f.elements[i].name] = f.elements[i].value;
-              }
-              res.action = f.action;
-              res.post = post;
-              return false;
-          }
+//Finish and return status code
+casper.run(function() {    
+    casper.exit(0); 
+});
 
-          var button = document.getElementById("download_csv");
-          button.click();
-          return res;
-      });
-
-      //console.log(JSON.stringify(res.post));
-	   var filename = "filename";	
-      //Use parameters to make post
-      casper.download(response.action, filename, "POST", response.post);
-  }
-];
-
-var index = 0
-
-setInterval(function () {
-    //Move to the next step depending on load state
-    if (!loading && typeof actions[index] == "function") {
-        console.log("action " + (index + 1));
-        actions[index]();
-        index++;
-    }
-
-    if (index == actions[index].length) {
-        console.log("Finished at " + index);
-        casper.run();
-    }
-}, 50);
+//Wait till page has loaded before moving to next step
+function PageReady()
+{
+	while(loading == true)
+	{
+		casper.wait(3000);
+	}	
+}
